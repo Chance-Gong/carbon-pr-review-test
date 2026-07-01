99ce78d499b71be07317ea11fba3307297f7ba19
Chance-Gong <chance.gong@ibm.com>
2026-07-01 11:26:44 -0500

    Add sample file 1 from Carbon PR 22577

---
+++ carbon-pr-test/1-index.ts
@@ -0,0 +1,13 @@
+// Sample from Carbon PR 22577
+// Original file: packages/utilities/src/index.ts
+// Status: modified
+// Changes: +0/-1
+
+@@ -6,7 +6,6 @@
+  */
+ 
+ export * from '@internationalized/number';
+-export type * from '@internationalized/number';
+ export * from './carousel';
+ export * from './datePartsOrder';
+ export * from './dateTimeFormat';
