d6ab454e4d37a849b29e9432a9b55a9b9f17cb4c
Chance-Gong <chance.gong@ibm.com>
2026-08-05 16:28:42 -0500

    Update packages/react/src/components/Notification/stories/StaticNotification.stories.js from Carbon PR 22864

---
+++ packages/react/src/components/Notification/stories/StaticNotification.stories.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright IBM Corp. 2016, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { StaticNotification } from '../../Notification';
+import { Link } from '../../Link';
+import { CodeSnippet } from '../../CodeSnippet';
+import mdx from './StaticNotification.mdx';
+
+export default {
+  title: 'Deprecated/preview__StaticNotification',
+  component: StaticNotification,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+export const Default = (args) => (
+  <>
+    <StaticNotification {...args} />
+
+    <div style={{ marginLeft: '.5rem', marginTop: '2rem' }}>
+      <p style={{ marginBottom: '1rem' }}>
+        Run the following codemod to automatically update usages in your
+        project:
+      </p>
+      <CodeSnippet type="single" feedback="Copied to clipboard">
+        npx @carbon/upgrade migrate refactor-to-callout --write
+      </CodeSnippet>
+    </div>
+  </>
+);
+
+Default.args = {
+  title: 'StaticNotification has been renamed to Callout',
+};
+
+Default.parameters = {
+  controls: {
+    include: ['title'],
+  },
+};
